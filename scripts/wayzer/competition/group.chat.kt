f98f5eb20c099b8399cc8afdf2c9681bb018cd02
nanmenyangde <nanmenyangde@foxmail.com>
2024-06-10 11:03:47 +0800

    聊天优化

--- scripts/wayzer/competition/group.chat.kt
+++ scripts/wayzer/competition/group.chat.kt
@@ -16,12 +16,20 @@ object GroupChat{
 
     fun Group.sendMessage(from: Player, message: String, team: Boolean) {
         for (p in Groups.player) {
-            val channel = when {
+            val channel = if (CompetitionService.gaming) when {
                 !team && from.team().id == 255 -> "[cyan][公屏][观战]"
                 !team -> "[cyan][公屏][[${from.team().coloredName()}]"
                 group[from.uuid()] != null && inSameGroup(setOf(from, p)) -> "[violet][[${group[from.uuid()]!!.name}[violet]队内]"
                 group[from.uuid()] == null && from.team() == teams.spectateTeam && p.team() == teams.spectateTeam -> "[violet][观战]"
                 group[from.uuid()] == null && from.team() == p.team() -> "[violet][[${from.team().chatName()}[violet]队内]"
+                p.team() == teams.spectateTeam -> "[violet][[${if(group[from.uuid()]==null) from.team().chatName() else group[from.uuid()]!!.name}[violet]队内]"
+                else -> continue
+            } else when {
+                !team && from.team().id == 255 -> "[cyan][公屏][观战]"
+                !team -> "[cyan][公屏][[${from.team().coloredName()}]"
+                group[from.uuid()] != null && inSameGroup(setOf(from, p)) -> "[violet][[${group[from.uuid()]!!.name}[violet]队内]"
+                group[from.uuid()] == null && from.team().id == 255 -> "[cyan][公屏][观战]"
+                group[from.uuid()] == null -> "[cyan][公屏][[${from.team().coloredName()}]"
                 p.team() == teams.spectateTeam -> "[violet][[${if(group[from.uuid()]==null) from.team().chatName() else group[from.uuid()]!!.name}[violet]队内]"
                 else -> continue
             }
